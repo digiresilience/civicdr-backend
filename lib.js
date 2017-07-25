fa791d44013e9c0b861d952df1451d5539ba3369
Seamus Tuohy <seamustuohy@users.noreply.github.com>
2017-07-25 16:57:40 -0400

    Add security headers to prevent stored XSS (#25)
    * Add security headers to prevent stored XSS
    * Added STS and XSS protection headers as well

--- lib.js
+++ lib.js
@@ -33,6 +33,15 @@ module.exports.init = function(opts) {
     next();
   });
 
+  /* Add Security Headers */
+  app.use(function(req, res, next) {
+    res.setHeader("X-Frame-Options", "DENY");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubdomains; preload");
+    res.setHeader("X-XSS-Protection", "1; mode=block");
+    next();
+  });
+
   /* Configure logger */
   const env = process.env.NODE_ENV;
   let logger = opts.logger;
