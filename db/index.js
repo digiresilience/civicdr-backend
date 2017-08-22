75dd0204eed33ac2f4e6bf115ba13ba783a308c8
Seamus Tuohy <seamustuohy@users.noreply.github.com>
2017-08-22 17:19:36 -0400

    Add SSL as default to DB (#36)

--- db/index.js
+++ db/index.js
@@ -1,5 +1,8 @@
 const environment = process.env.NODE_ENV || 'user';
 const config = require('../knexfile.js')[environment];
+var pg = require('pg');
+
+pg.defaults.ssl = true;
 
 const db = require('knex')(config);
 
