283b51fbc48f767faac8c3385cdd90fd2bba566a
Vagrant Default User <vagrant@dsx-civicdr>
2018-03-10 09:08:13 +0000

    Fix JWT verification

--- db/index.js
+++ db/index.js
@@ -2,6 +2,7 @@ const environment = process.env.NODE_ENV || 'user';
 const config = require('../knexfile.js')[environment];
 var pg = require('pg');
 
+
 pg.defaults.ssl = true;
 
 const db = require('knex')(config);
